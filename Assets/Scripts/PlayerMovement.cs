fed92fcb0ece47258c554107a9ac28f183a98ed0
Baran <kilexey@gmail.com>
2020-11-14 16:48:48 +0000

    added 4 way animation (slightly bugged)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed; // make it public so we can change it from the inspector
     private Rigidbody2D myRigidbody; // make a reference to myRigidbody
     private Vector3 change; // how much we want the player's position to change
+    private Animator animator;// reference to the animator component
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
         
     }
@@ -22,11 +24,26 @@ public class PlayerMovement : MonoBehaviour
         change.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed; // if player pushes any of the right/left buttons, update "x" coord
         change.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
         //Debug.Log(change); logs/prints the change
+        UpdateAnimationAndMove();
+        
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             transform.Translate(new Vector3(change.x, change.y));
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        } else {
+            animator.SetBool("moving", false);
         }
     }
+
+}
+
+
 
     /*
     void MoveCharacter() // move the character from other places (i.e buttons on the screen), so we create is as a method
@@ -34,4 +51,4 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.deltaTime); // grab the pos of the player, add change (x speed) and multiply how much time has passed time has passed since the prev frame
     }
     */
-}
+
