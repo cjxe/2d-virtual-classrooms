7ffaea457824509944953bf646a2bc05b0c4ba54
Baran <kilexey@gmail.com>
2020-11-15 02:53:56 +0000

    join breakout rooms button open a panel

---
+++ Assets/Scripts/OpenWindowBreakoutRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWindowBreakoutRoom : MonoBehaviour
+{
+    public GameObject Window;
+
+    public void OpenWindowBR()
+    {
+        if(Window != null)
+        {
+            bool isActive = Window.activeSelf;
+
+            Window.SetActive(!isActive);
+        }
+    }
+}
+
+// https://www.youtube.com/watch?v=LziIlLB2Kt4
